3b0524fa088c3dec332f93b2c33c2f105dfd8fb6
Arthur <a.dhuy@orange.fr>
2019-10-31 00:33:57 +0100

    add ajax

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,15 @@
 class CocktailsController < ApplicationController
   def index
     if params["search"]
-      @filter = params["search"]["flavors"].concat(params["search"]["strengths"]).flatten.reject(&:blank?)
+      @filter = params["search"]["categories"].concat(params["search"]["flavors"]).flatten.reject(&:blank?)
       @cocktails = @filter.empty? ? Cocktail.all : Cocktail.all.tagged_with(@filter, any: true)
+      # @cocktails = Cocktail.all.global_search("#{@filter}")
     else
       @cocktails = Cocktail.all
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 end
